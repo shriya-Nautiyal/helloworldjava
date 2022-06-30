90e5756b0aff0b78cbd010ac385123816e301243
shriya-Nautiyal <106568764+shriya-Nautiyal@users.noreply.github.com>
2022-07-01 00:17:32 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 19th june - 8:22 AM!");
+    System.out.println("Hello World 19th june - 8:25 AM!");
     }
 }
